8d8f99cbd0e4c0fdf6fa60d43440f29983e9e510
rodrisebok <rodrigosebok@gmail.com>
2025-05-14 10:48:03 -0300

    Se modificó FormularioNuevaTarifa, agregandole un mensaje de exito y la redireccion a la pagina principal luego de guardarlo

--- src/components/FormularioNuevaTarifa.tsx
+++ src/components/FormularioNuevaTarifa.tsx
@@ -1,53 +1,73 @@
-import React from 'react';
+
 import '../CrearTarifa.css';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 
-const FormularioNuevaTarifa: React.FC = () => (
+const FormularioNuevaTarifa: React.FC = () => {
+  const [mostrarMensaje, setMostrarMensaje] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('Formulario guardado (simulado)');
+    setMostrarMensaje(true);
+    setTimeout(() => {
+      navigate('/'); 
+    }, 1200);
+  };
+
+  return (
     <div className="crear-tarifa">
-        <h2>Gestionar Tarifa para nuevo viaje</h2>
-        <form className="formulario-tarifa">
+      <h2>Gestionar Tarifa para nuevo viaje</h2>
+      <form className="formulario-tarifa" onSubmit={handleSubmit}>
+        <label>Tipo de Vehículo</label>
+        <select>
+          <option>Camioneta</option>
+          <option>Camión Mediano</option>
+          <option>Camión Grande</option>
+        </select>
 
-            <label>Tipo de Vehículo</label>
-            <select>
-                <option>Camioneta</option>
-                <option>Camión Mediano</option>
-                <option>Camión Grande</option>
-            </select>
+        <label>Zona</label>
+        <select>
+          <option>AMBA</option>
+          <option>BsAs-Rosario</option>
+        </select>
 
-            <label>Zona</label>
-            <select>
-                <option>AMBA</option>
-                <option>BsAs-Rosario</option>
-            </select>
+        <label>Tipo de Carga</label>
+        <select>
+          <option>Regular</option>
+          <option>Refrigerada</option>
+          <option>Peligrosa</option>
+        </select>
 
-            <label>Tipo de Carga</label>
-            <select>
-                <option>Regular</option>
-                <option>Refrigerada</option>
-                <option>Peligrosa</option>
-            </select>
+        <label>Peso Estimado</label>
+        <input type="text" />
 
-            <label>Peso Estimado</label>
-            <input type="text" />
+        <label>Volumen Estimado</label>
+        <input type="number" />
 
-            <label>Volumen Estimado</label>
-            <input type="number" />
+        <label> Requisitos especiales</label>
+        <input type="text" />
 
-            <label> Requisitos especiales</label>
-            <input type="text" />
+        <label>Transportista</label>
+        <input type="text" placeholder="Nombre de la empresa" />
 
-            <label>Transportista</label>
-            <input type="text" placeholder="Nombre de la empresa" />
+        <label>Costo Base</label>
+        <input type="number" placeholder="$" />
 
-            <label>Costo Base</label>
-            <input type="number" placeholder="$" />
+        <label>Adicionales</label>
+        <select>
+        </select>
 
-            <label>Adicionales</label>
-            <select>
-            </select>
+        <button type="submit">Guardar Tarifa</button>
+      </form>
 
-            <button type="submit">Guardar Tarifa</button>
-        </form>
+      {mostrarMensaje && (
+        <div className="mensaje-exito">
+          Su registro se ha guardado con éxito!
+        </div>
+      )}
     </div>
-);
+  );
+};
 
 export default FormularioNuevaTarifa;
