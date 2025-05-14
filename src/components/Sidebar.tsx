a124a83b935ade261f8aa3159d0f2d504100bf76
rodrisebok <rodrigosebok@gmail.com>
2025-05-14 11:04:54 -0300

    Linkeos al Menu y las Tarifas del panel izquierdo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Sidebar: React.FC = () => (
   <aside className="sidebar">
-    <h2>Menú</h2>
-    <a href="#">Dashboard</a>
-    <a href="#">Tarifas</a>
-    <a href="#">Adicionales</a>
-    <a href="#">Reportes</a>
-    <a href="#">Configuración</a>
-    <a href="#">Cerrar sesión</a>
+    <h2><Link to="/">Menú</Link></h2>
+    <Link to="/">Dashboard</Link>
+    <Link to="/crear-tarifa">Tarifas</Link>
+    <Link to="/">Adicionales</Link>
+    <Link to="/">Reportes</Link>
+    <Link to="/">Configuración</Link>
+    <Link to="/">Cerrar Sesión</Link>
   </aside>
 );
 
