58b427883ccfecdcebc6b0bdfd0d60b913cc6ba4
rodrisebok <rodrigosebok@gmail.com>
2025-06-27 12:46:42 -0300

    Primera instancia de reportes generales con catalogo de adicionales

--- src/Reportes.tsx
+++ src/Reportes.tsx
@@ -1,8 +1,10 @@
+
 import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import { HeaderConMenu } from './components/Header';
+import CatalogoAdicionales from './components/reportes/CatalogoAdicionales'; 
 import './css/App.css';
-import './css/CrearTarifa.css';
+import './css/CrearTarifa.css'; 
 
 const Reportes: React.FC = () => {
     const [sidebarAbierta, setSidebarAbierta] = useState(false);
@@ -13,14 +15,13 @@ const Reportes: React.FC = () => {
 
     return (
         <div className="app">
-            
             <HeaderConMenu onImagenClick={toggleSidebar} /> 
-
             
             {sidebarAbierta && <Sidebar isOpen={sidebarAbierta} toggleSidebar={toggleSidebar} />} 
             
             <div className="content-area">
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex est optio rerum, animi dolorum hic repellendus! Corrupti odit possimus illum labore ea nobis cum distinctio voluptatibus? Exercitationem quia dolorem quisquam?</h1>
+                <h1>Reportes Generales</h1>
+                <CatalogoAdicionales /> 
             </div>
         </div>
     );
